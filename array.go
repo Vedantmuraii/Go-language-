88a77a294e2f65a0a444ad6fca3c9e2e14909c70
Vedantmuraii <115358120+Vedantmuraii@users.noreply.github.com>
2024-04-19 22:48:34 +0530

    Add files via upload

---
+++ array.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+// func PrintArray(arr [5]int) {
+// 	fmt.Println("Array Elements: ")
+// 	for i := 0; i < len(arr); i++ {
+// 		fmt.Println()
+// 	}
+// }
+
+func main() {
+	var arrayOfInteger = [5]int{1, 5, 8, 0, 3}
+	fmt.Println(arrayOfInteger)
+}
