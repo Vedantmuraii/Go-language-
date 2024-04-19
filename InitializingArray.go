46544ba97410ae6ab4eb0b1d4e351304bc169354
Vedantmuraii <115358120+Vedantmuraii@users.noreply.github.com>
2024-04-19 22:48:45 +0530

    Add files via upload

---
+++ InitializingArray.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var arrayOfIntegers [3]int
+	// arrayOfInteger[0] = 5
+	// arrayOfInteger[1] = 10
+	// arrayOfInteger[2] = 15
+
+	// fmt.Println(arrayOfIntegers)
+
+	// arrayOfIntegers := [5]int{0: 7, 3: 9}
+	// fmt.Println(arrayOfIntegers)
+
+	var arrayOfString = [...]string{"Hello", "Rajesh"}
+	fmt.Println(arrayOfString)
+}
