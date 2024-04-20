3f97232d950b470585d2176efefb98f1b6ec1a5b
Vedantmuraii <115358120+Vedantmuraii@users.noreply.github.com>
2024-04-20 22:15:53 +0530

    Add files via upload

---
+++ ChnagetoTitleCase.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	str1 := "Hello How Are You"
+	str2 := "Hii I Am Fine"
+
+	fmt.Println(strings.Title(str1))
+	fmt.Println(strings.Title(str2))
+}
