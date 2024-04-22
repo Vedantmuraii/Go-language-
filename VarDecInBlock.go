0a3951e6df06204fafb9735f43697728849cc98f
Vedantmuraii <115358120+Vedantmuraii@users.noreply.github.com>
2024-04-22 13:19:22 +0530

    Add files via upload

---
+++ VarDecInBlock.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+var d = 3
+
+func main() {
+	var (
+		a int
+		b int    = 1
+		c string = "Hello"
+	)
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Println(d)
+}
