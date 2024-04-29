7c81a635a1b7bc8bff261332f0d0a07c341e666f
Vedantmuraii <115358120+Vedantmuraii@users.noreply.github.com>
2024-04-29 17:59:13 +0530

    Add files via upload

---
+++ SleepFunc.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println("hello world")
+	time.Sleep(500 * time.Millisecond)
+	fmt.Println("hello india")
+	time.Sleep(8 * time.Second)
+	fmt.Println("hello, welcome to golang class")
+}
