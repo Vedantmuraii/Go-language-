702bc5a4e99d17cb8006e7263457cf1c066a75d5
Vedantmuraii <115358120+Vedantmuraii@users.noreply.github.com>
2024-04-29 17:58:27 +0530

    Add files via upload

---
+++ ContainsAnyFunc.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	str1 := "india"
+	str2 := "australia"
+	str3 := "america"
+	str4 := "canada"
+
+	fmt.Println(strings.ContainsAny(str1, "iu"))
+	fmt.Println(strings.ContainsAny(str2, "iu"))
+	fmt.Println(strings.ContainsAny(str3, "iu"))
+	fmt.Println(strings.ContainsAny(str4, "iu"))
+
+	str1 := "INDIA"
+	str2 := "AUSTRALIA"
+
+	FMT.PRINTLN(STRINGS.CONTAINSRUNE(STR1, 65))
+}
